ea64500c417405512f23ca4fefe47e16baf4b1d0
Oleg <oleg.kobzarius@gmail.com>
2019-08-20 22:43:24 +0300

    Начало работы над классом well для проведения массовых расчетов

--- well_run_by_API.py
+++ well_run_by_API.py
@@ -77,10 +77,10 @@ def calc_well_plin_pwf_atma_for_fsolve(c_calibr_head_d):
     print(plin_calculated_atm)
     return result_for_folve
 
-fsolve(calc_well_plin_pwf_atma_for_fsolve, 0.5, xtol = 0.5)
+#fsolve(calc_well_plin_pwf_atma_for_fsolve, 0.5, xtol = 0.5)
 from scipy.optimize import minimize
 print("     \n")
-minimize(calc_well_plin_pwf_atma_for_fsolve, [1], bounds = [[0,1]])
+minimize(calc_well_plin_pwf_atma_for_fsolve, [0.5], bounds = [[0,1]])
 print(str(c_calibr_head_d) + " c_calibr_head_d")
 
 
@@ -90,5 +90,85 @@ print(str(p_wellhead_data_atm) +" p_wellhead_data_atm ")
 for i in range(len(result_fsolve[0])):
     print(str(result_fsolve[1][i]) + " -  " + str(result_fsolve[0][i]))
 
+import pandas as pd
+
+class well():
+    def __init__(self):
+        self.result = None
+        self.gamma_oil = 0.945
+        self.gamma_gas = 0.9
+        self.gamma_wat = 1.011
+        self.rsb_m3m3 = 29.25
+        self.tres_c = 16
+        self.pb_atm = 40
+        self.bob_m3m3 = 1.045
+        self.muob_cp = 400
+        self.ksep_d = 0.7
+        self.psep_atm = 30
+        self.tsep_c = 30
+        self.d_choke_mm = 8
+        self.dcas_mm = 160
+        self.h_tube_m = 830
+        self.d_tube_mm = 75
+        self.Power_motor_nom_kWt = 140
+        self.ESP_head_nom = 1500
+        self.ESP_rate_nom = 320
+        self.ESP_freq = 38
+
+        self.rp_m3m3 = 30
+
+        self.p_intake_data_atm = 29.93
+        self.p_wellhead_data_atm = 22.70
+        self.p_buf_data_atm = 27.0
+        self.p_wf_atm = 29.93
+        self.p_cas_data_atm = 25.90
+
+        self.eff_motor_d = 0.89
+        self.i_motor_nom_a = 6
+        self.power_motor_nom_kwt = 140
+        self.i_motor_data_a = 42.94
+        self.cos_phi_data_d = 0.70818
+        self.load_motor_data_d = 0.55957
+        self.u_motor_data_v = 1546.65
+        self.active_power_cs_data_kwt = 81.297297
+
+        self.qliq_m3day = 122.2
+        self.watercut_perc = 25.6
+
+        self.c_calibr_head_d = 1
+
+    def calc_well_plin_pwf_atma_for_fsolve(self, c_calibr_head_d):
+        self.c_calibr_head_d = float(c_calibr_head_d)
+        self.PVTstr = UniflocVBA.calc_PVT_encode_string(self.gamma_gas, self.gamma_oil, self.gamma_wat, self.rsb_m3m3,
+                                                   self.rp_m3m3,
+                                                   self.pb_atm, self.tres_c, self.bob_m3m3, self.muob_cp,
+                                                   ksep_fr=self.ksep_d,
+                                                   pksep_atma=self.psep_atm,
+                                                   tksep_C=self.tsep_c)
+        self.Wellstr = UniflocVBA.calc_well_encode_string(831, 830, 0, self.dcas_mm, self.d_tube_mm, self.d_choke_mm,
+                                                     tbh_C=self.tres_c)
+
+        self.esp_id = UniflocVBA.calc_ESP_id_by_rate(320)
+
+        self.ESPstr = UniflocVBA.calc_ESP_encode_string(self.esp_id, self.ESP_head_nom, self.ESP_freq,
+                                                   self.u_motor_data_v, self.power_motor_nom_kwt,
+                                                   self.tsep_c, ESP_Hmes_m=self.h_tube_m,
+                                                   c_calibr_power=1,
+                                                   c_calibr_rate=1)
+        self.result = UniflocVBA.calc_well_plin_pwf_atma(self.qliq_m3day, self.watercut_perc, self.p_wf_atm,
+                                                         self.p_cas_data_atm, self.Wellstr,
+                                                    self.PVTstr, self.ESPstr, c_calibr_head_d=self.c_calibr_head_d)
+
+        self.plin_calculated_atm = result[0][0]
+        result_out = (self.plin_calculated_atm - self.p_wellhead_data_atm) ** 2
+        return result_out
+
+    def calc_well(self):
+        fsolve(self.calc_well_plin_pwf_atma_for_fsolve, 0.5, xtol=1)
+        minimize(self.calc_well_plin_pwf_atma_for_fsolve, [0.5], bounds=[[0,100]])
+
+well_1354 = well()
+well_1354.calc_well()
+print(well_1354.result)
 
 
